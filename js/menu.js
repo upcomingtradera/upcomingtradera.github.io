ed546227509df67f5395709fbed0bd102d0bf055
upcomingtrader <contact@upcomingtrader.com>
2023-10-05 15:04:48 +0800

    added toggle table of contents button

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,52 @@
 'use strict';
 
 (function iifeMenu(document, window, undefined) {
-	var menuBtn = document.querySelector('.menu__btn');
-	var	menu = document.querySelector('.menu__list');
+    var menuBtn = document.querySelector('.menu__btn');
+    var menu = document.querySelector('.menu__list');
+    
+    // Define the TOC button and menu elements
+    var tocBtn = document.querySelector('.toc__btn');
+    var tocMenu = document.querySelector('.toc__menu');
 
-	function toggleMenu() {
-		menu.classList.toggle('menu__list--active');
-		menu.classList.toggle('menu__list--transition');
-		this.classList.toggle('menu__btn--active');
-		this.setAttribute(
-			'aria-expanded',
-			this.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
-		);
-	}
+    function toggleMainMenu(targetBtn, targetMenu) {
+        targetMenu.classList.toggle('menu__list--active');
+        targetMenu.classList.toggle('menu__list--transition');
+        targetBtn.classList.toggle('menu__btn--active');
+        targetBtn.setAttribute(
+            'aria-expanded',
+            targetBtn.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
+        );
+    }
 
-	function removeMenuTransition() {
-		this.classList.remove('menu__list--transition');
-	}
+    function toggleTOC(targetBtn, targetMenu) {
+        targetMenu.classList.toggle('toc__menu--active');
+        targetBtn.classList.toggle('toc__btn--active');
+        targetBtn.setAttribute(
+            'aria-expanded',
+            targetBtn.getAttribute('aria-expanded') === 'true' ? 'false' : 'true'
+        );
+    }
 
-	if (menuBtn && menu) {
-		menuBtn.addEventListener('click', toggleMenu, false);
-		menu.addEventListener('transitionend', removeMenuTransition, false);
-	}
+    function removeMenuTransition() {
+        this.classList.remove('menu__list--transition');
+    }
+
+    function removeTOCTransition() {
+        this.classList.remove('toc__menu--transition');
+    }
+
+    if (menuBtn && menu) {
+        menuBtn.addEventListener('click', function() {
+            toggleMainMenu(menuBtn, menu);
+        }, false);
+        menu.addEventListener('transitionend', removeMenuTransition, false);
+    }
+
+    // New TOC button event listeners
+    if (tocBtn && tocMenu) {
+        tocBtn.addEventListener('click', function() {
+            toggleTOC(tocBtn, tocMenu);
+        }, false);
+        tocMenu.addEventListener('transitionend', removeTOCTransition, false);
+    }
 }(document, window));
